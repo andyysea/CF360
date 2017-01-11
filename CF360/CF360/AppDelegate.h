3b695bf60ff36127a09f6d2c56983c1b81976f62
andyysea <601503988@qq.com>
2017-01-11 15:53:51 +0800

    创建项目,以及分类文件

---
+++ CF360/CF360/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CF360
+//
+//  Created by junde on 2017/1/11.
+//  Copyright © 2017年 junde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
