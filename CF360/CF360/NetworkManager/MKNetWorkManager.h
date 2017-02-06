c68e8f216aadcb8bb3c98092a60011da887b04f6
andyysea <601503988@qq.com>
2017-02-06 14:54:50 +0800

    封装MKNetwork

---
+++ CF360/CF360/NetworkManager/MKNetWorkManager.h
@@ -0,0 +1,13 @@
+//
+//  MKNetWorkManager.h
+//  CF360
+//
+//  Created by junde on 2017/2/6.
+//  Copyright © 2017年 junde. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKNetWorkManager : NSObject
+
+@end
