f9632a1552af0f1589cc85e9c0fa590e1b677c2f
andyysea <601503988@qq.com>
2017-02-13 11:52:50 +0800

    修改颜色分类文件路劲,并且添加常量文件,可以添加通知,或者其他常量

---
+++ CF360/CF360/Other/Common/CFConst.h
@@ -0,0 +1,17 @@
+//
+//  CFConst.h
+//  CF360
+//
+//  Created by junde on 2017/2/13.
+//  Copyright © 2017年 junde. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//extern 让外部的文件可以访问
+//UIKIT_EXTERN: 自动区分C语言和C++
+
+// 登陆成功的通知 --> 用来判断左边是否显示用户账户的通知
+UIKIT_EXTERN NSString *const CFLogInSuccessNotifacation;
+
---
+++ CF360/CF360/Other/Common/CFConst.m
@@ -0,0 +1,14 @@
+//
+//  CFConst.m
+//  CF360
+//
+//  Created by junde on 2017/2/13.
+//  Copyright © 2017年 junde. All rights reserved.
+//
+
+#import "CFConst.h"
+
+
+// 登陆成功的通知 --> 用来判断左边是否显示用户账户的通知
+NSString *const CFLogInSuccessNotifacation = @"CFLogInSuccessNotifacation";
+
